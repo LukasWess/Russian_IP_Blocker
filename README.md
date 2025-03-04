e190021d2c6c666a425f0f30f6d100d179abf398
Lukas Wessels <lukas25@live.no>
2025-03-04 14:54:52 +0100

    Add Russian IP blocking middleware and update README with setup instructions

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Russian_IP_Blocker
+To set this up:
+
+Include the given code into your app.js file. Remember to require the package. 
+
+Install required packages:
+
+
+npm install geoip-lite
+
+Download the GeoLite2 database:
+
+Create a free MaxMind account at https://www.maxmind.com/
+
+Download the GeoLite2 Country database (GeoLite2-Country.mmdb)
+
+Place the database file in your project's root directory
---
+++ app.js
@@ -0,0 +1,18 @@
+const geoip = require('geoip-lite');
+
+
+// Middleware to block Russian IPs
+app.use((req, res, next) => {
+    // Get client IP address
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    
+    // Perform GeoIP lookup
+    const geo = geoip.lookup(ip);
+    
+    // Block if the country is Russia (RU)
+    if (geo && geo.country === 'RU') {
+        return res.status(403).send('Access denied - Russian IP blocked');
+    }
+    
+    next();
+});
